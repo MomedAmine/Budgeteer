32dde48ad2f49cae79775331dce385d10ff3f793
Momed PC <elmaghraoui.mohamedelamine@gmail.com>
2025-01-29 21:55:09 +0100

    finally in sync

--- Budgeteer/src/App.jsx
+++ Budgeteer/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Header from './assets/Header'
+import Welcome from './assets/Welcome'
+// import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'// import './App.css'
 
 function App() {
@@ -7,7 +9,8 @@ function App() {
 
   return (
     <>
-    
+    <Header/>
+    <Welcome/>
     </>
   )
 }
