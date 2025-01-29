270db9a8909b8abb30890d0944cdac765db2ff96
Momed PC <elmaghraoui.mohamedelamine@gmail.com>
2025-01-29 23:29:58 +0100

    New Add Category dynamic

--- Budgeteer/src/assets/AddCategory.jsx
+++ Budgeteer/src/assets/AddCategory.jsx
@@ -3,47 +3,25 @@ import AddExpenceCategory from "./AddExpenceCategory";
 import AddIncomeCategory from "./AddIncomeCategory";
 
 function AddCategory(props) {
-  const [InOut, setInOut] = useState(false);
-  const changeInOut = () => {
-    setInOut(!InOut);
+  const [InOut, setInOut] = useState(true);
+  const changeInOut = (bool) => {
+    setInOut(bool);
   };
   return (
     <div id="addCategoryContainer">
-      <div id="InOutDiv">
-        <div id="InOutlabels" onClick={changeInOut}>
-          <label
-            style={{
-              borderStyle: "solid",
-              /*InOut ? "solid solid none solid" : "none"*/
-              borderRadius: "25px 0px 0px 0px",
-              borderColor: InOut
-                ? "black black transparent black"
-                : "transparent transparent black transparent",
-            }}
-          >
-            Income
-          </label>
-          <input
-            type="checkbox"
-            name="InOutCheckBox"
-            id="InOutCheckBox"
-            checked={InOut}
-          />
-          <label
-            style={{
-              borderStyle: "solid",
-              borderRadius: " 0px 25px 0px 0px",
-              borderColor: !InOut
-                ? "black black transparent black"
-                : "transparent transparent black transparent",
-            }}
-          >
-            Expence
-          </label>
+      <div id ="AddIncomeDiv" className="AddCategoryDiv">
+        <div id="AddIncomeHead" className="AddCategoryHead" onClick={()=>changeInOut(true)}>
+          <h3> INCOME </h3>
         </div>
-        <div id="addCategoryFormDiv">
-          {InOut ? <AddIncomeCategory /> : <AddExpenceCategory />}
+        <div id="AddIncomeForm" style={{transition:"0.3s",overflow :InOut?"":"hidden",width:"100%",height : InOut?"150px":"0px",}}>
+          <AddIncomeCategory/>
         </div>
+      </div>
+      <div id ="AddExpenseDiv" className="AddCategoryDiv">
+        <div id="AddExpenceHead" className="AddCategoryHead" onClick={()=>changeInOut(false)}>
+          <h3> EXPENCE </h3>
+        </div>
+        <div id="AddIncomeForm" style={{transition:"0.3s",height:"50px",width:"50px",backgroundColor:"#222",height : InOut?"0px":"150px",}}></div>
       </div>
     </div>
   );
